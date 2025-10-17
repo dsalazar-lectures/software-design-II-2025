2ff692ccc869d02d2df89e9df9d50813960c2501
eduardo.sancho <eduardo.sancho@ucr.ac.cr>
2025-10-17 07:39:50 -0600

    se hicieron abstractos los métodos de PizzaBuilder

--- lab3/builder/src/PizzaBuilder.py
+++ lab3/builder/src/PizzaBuilder.py
@@ -1,15 +1,22 @@
-class PizzaBuilder:
+from abc import ABC, abstractmethod
+
+class PizzaBuilder(ABC):
+    @abstractmethod
     def make_dough(self):
         pass
 
+    @abstractmethod
     def add_sauce(self):
         pass
 
+    @abstractmethod
     def add_cheese(self):
         pass
 
+    @abstractmethod
     def add_toppings(self):
         pass
 
+    @abstractmethod
     def get_pizza(self):
         pass
