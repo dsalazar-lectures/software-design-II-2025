ebffb216dfbac6dfc8766517bf400da95dcee26a
eduardo.sancho <eduardo.sancho@ucr.ac.cr>
2025-10-17 04:07:46 -0600

    created classes Pizza (product) and PizzaBuilder (base builder)

---
+++ lab3/builder/src/PizzaBuilder.py
@@ -0,0 +1,15 @@
+class PizzaBuilder:
+    def make_dough(self):
+        pass
+
+    def add_sauce(self):
+        pass
+
+    def add_cheese(self):
+        pass
+
+    def add_toppings(self):
+        pass
+
+    def get_pizza(self):
+        pass
